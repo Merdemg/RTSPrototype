47bcfb327642a86fadd7de67f9cb87e884602d23
Merdemg <merdemgunay88@gmail.com>
2025-05-04 14:58:48 -0400

    Keeping track of num of enemies spawned & eliminated

--- Assets/Scripts/Managers/ScoreManager.cs
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -6,9 +6,14 @@ public class ScoreManager : MonoBehaviour
     public static ScoreManager Instance { get; private set; }
 
     public static event Action<int> OnScoreChanged;
+    public static event Action<int, int> OnEnemyStatsChanged; // killed, total
 
     private int currentScore = 0;
+    private int enemiesKilled = 0;
+    private int enemiesSpawned = 0;
     public int CurrentScore => currentScore;
+    public int EnemiesKilled => enemiesKilled;
+    public int EnemiesSpawned => enemiesSpawned;
 
     private void Awake()
     {
@@ -20,25 +25,42 @@ public class ScoreManager : MonoBehaviour
         Instance = this;
 
         UnitEvents.OnUnitDied += HandleUnitDeath;
+        UnitEvents.OnUnitSpawned += HandleSpawned;
     }
 
     private void OnDestroy()
     {
         UnitEvents.OnUnitDied -= HandleUnitDeath;
+        UnitEvents.OnUnitSpawned -= HandleSpawned;
     }
 
     private void HandleUnitDeath(Unit unit)
     {
         if (unit.Faction == Faction.Enemy)
         {
+            enemiesKilled++;
             currentScore += unit.PointValue;
             OnScoreChanged?.Invoke(currentScore);
+            OnEnemyStatsChanged?.Invoke(enemiesKilled, enemiesSpawned);
         }
     }
 
-    public void ResetScore()
+    private void HandleSpawned(Unit unit)
+    {
+        if (unit.Faction == Faction.Enemy)
+        {
+            enemiesSpawned++;
+            OnEnemyStatsChanged?.Invoke(enemiesKilled, enemiesSpawned);
+        }
+    }
+
+    public void ResetStats()
     {
         currentScore = 0;
+        enemiesKilled = 0;
+        enemiesSpawned = 0;
+
         OnScoreChanged?.Invoke(currentScore);
+        OnEnemyStatsChanged?.Invoke(enemiesKilled, enemiesSpawned);
     }
 }
---
+++ Assets/Scripts/UI/EndGameCanvasController.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using TMPro;
+using System.Collections.Generic;
+
+public class EndGameCanvasController : MonoBehaviour
+{
+    [SerializeField] Canvas endGameCanvas;
+    [SerializeField] GameObject victoryPanel;
+    [SerializeField] GameObject defeatPanel;
+    [SerializeField] List<TMP_Text> enemiesEliminatedTextList = new List<TMP_Text>();
+
+    private void OnEnable()
+    {
+        GameStateManager.OnGameStateChanged += HandleStateChanged;
+        ScoreManager.OnEnemyStatsChanged += UpdateEnemiesEliminated;
+    }
+
+    private void OnDisable()
+    {
+        GameStateManager.OnGameStateChanged -= HandleStateChanged;
+        ScoreManager.OnEnemyStatsChanged -= UpdateEnemiesEliminated;
+    }
+
+    private void HandleStateChanged(GameState state)
+    {
+        endGameCanvas.enabled = state == GameState.Won || state == GameState.Lost;
+        victoryPanel.SetActive(state == GameState.Won);
+        defeatPanel.SetActive(state == GameState.Lost);
+
+        if (state == GameState.Won || state == GameState.Lost)
+        {
+            UpdateEnemiesEliminated(ScoreManager.Instance.EnemiesKilled, ScoreManager.Instance.EnemiesSpawned);
+        }
+    }
+
+    private void UpdateEnemiesEliminated(int killed, int total)
+    {
+        foreach (var text in enemiesEliminatedTextList)
+        {
+            text.text = $"Enemies Eliminated: {killed} / {total}";
+        }
+    }
+}
